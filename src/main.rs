54fd8755b30d9600e4d5c0332e95760b93cdfd90
Hlebotron <stabasov@gmail.com>
2024-08-25 15:07:15 +0000

    v0.2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use local_ip_address::local_ip;
 use tiny_http::{Server, Request, Method, Response};
 use std::process::exit;
-use std::fs::File;
+use std::fs::{File, copy, remove_file};
+use std::fs::read_to_string;
 use std::net::{IpAddr::V4, Ipv4Addr};
 use std::env::args;
-use std::io::{Read, Write};
+use std::io::Write;
+//use std::fs::write;
 use std::thread::sleep;
 use std::time::Duration;
+use std::path::Path;
 
 ///The constant SRC_DIR is for setting the path of the directory "src", where all the files are.
 ///
@@ -68,10 +71,33 @@ fn start_server(address: &str, port: &str) -> Result<(), ()> {
             (Method::Get, "/favicon.ico") => {
                 serve("favicon.ico", request);
             }
+            //TODO
             (Method::Post, "/validate") => {
-                post("data", "secretsauce"); 
-                println!("{}", get_request_content(&mut request).unwrap());
-                request.respond(Response::from_string("pass".to_string()));
+                let content = get_request_content(&mut request).unwrap();
+                println!("Content: {}", &content);
+                if !is_contained("nameList", &content) {
+                    post("nameList", &content); 
+                    println!("Success: {}", &content);
+                    remove_file("nameList.swap");
+                    request.respond(Response::from_string("success".to_string()));
+                } else {
+                    request.respond(Response::from_string("fail".to_string()));
+                    println!("Already registered: {}", &content);
+                }
+            }
+            //TODO
+            (Method::Put, "/changeName") => {
+                let content = get_request_content(&mut request).unwrap();
+                let splitContent: Vec<&str> = content.split("&").collect();
+                let name = splitContent[0];
+                let newName = splitContent[1];
+                println!("name: {name}, newname: {newName}");
+                if is_contained("nameList", name) {
+                    change_name("nameList", name, newName);
+                } else {
+                    post("nameList", newName); 
+                }
+                request.respond(Response::from_string("200".to_string()));
             }
             _ => {
                 eprintln!("ERROR: Invalid request: {}", request.url());
@@ -83,34 +109,126 @@ fn start_server(address: &str, port: &str) -> Result<(), ()> {
 }
 fn serve(filename: &str, request: Request) {
     let path = format!("{SRC_DIR}/{filename}");
-    let file = File::options().read(true).open(&path).unwrap_or_else(|err| {
-        eprintln!("ERROR: Could not open file: {err}"); 
-        File::create(filename).unwrap()
-    });
+    let file = File::options()
+        .read(true)
+        .open(&path)
+        .unwrap_or_else(|err| {
+            eprintln!("ERROR: Could not open file: {err}"); 
+            File::create(filename).unwrap()
+        });
     let _ = request.respond(Response::from_file(file)).map_err(|err| {
         eprintln!("ERROR: Could not respond: {err}");
     });
 }
 fn post(filename: &str, content: &str) {
     let path = format!("{SRC_DIR}/{filename}");
-    let mut file = File::options().append(true).open(&path).unwrap_or_else(|err| {
-        eprintln!("ERROR: Could not open file: {err}"); 
-        File::create(filename).unwrap()
-    });
+    let swap_path = format!("{SRC_DIR}/{filename}.swap");
+    copy(&path, &swap_path);
+    let mut file = File::options()
+        .append(true)
+        .open(&swap_path)
+        .unwrap_or_else(|err| {
+            eprintln!("ERROR: Could not open file: {err}"); 
+            File::create(filename).unwrap()
+        });
     write!(file, "{}\n", content);
+    copy(&swap_path, &path);
 }
-fn validate(filename: &str, name: &str) -> Result<bool, ()> {
+//TODO
+fn is_contained(filename: &str, name: &str) -> bool {
+    let swap_path = format!("{SRC_DIR}/{filename}.swap");
     let path = format!("{SRC_DIR}/{filename}");
-    let mut file = File::options().read(true).open(&path).unwrap_or_else(|err| {
+    let file_content = read_to_string(path).unwrap_or_else(|err| {
         eprintln!("ERROR: Could not open file: {err}"); 
-        File::create(filename).unwrap()
+        File::create(filename.to_string() + ".swap").unwrap();
+        "".to_string()
     });
-    let mut file_content: String = Default::default();
-    file.read_to_string(&mut file_content);
-    Ok(true)
+    println!("{file_content}");
+    //println!("File content: {lines:?}", lines = file_content.lines().collect::<Vec<_>>());
+    println!("Name: {name}");
+    let mut is_contained: bool = false;
+    let is_contained_vec: Vec<bool> = file_content
+        .lines()
+        .map(|line| {
+            if line.to_lowercase() == name.to_lowercase() {
+                return true
+            } else {
+                return false
+            }
+        })
+        .collect();
+    for boolean in is_contained_vec {
+        if boolean {
+            is_contained = true;
+        }
+    }
+    is_contained
 }
 fn get_request_content(request: &mut Request) -> Result<String, ()> {
     let mut buffer: String = Default::default();
     let _ = request.as_reader().read_to_string(&mut buffer);
     Ok(buffer)
+}
+//TODO
+fn change_name(filename: &str, name: &str, newName: &str) {
+    //let swap_path = format!("{SRC_DIR}/{filename}.swap");
+    let path = format!("{SRC_DIR}/{filename}");
+    //copy(&path, &swap_path);
+    let file_content = read_to_string(&path).unwrap_or_else(|err| {
+        eprintln!("ERROR: Could not open file: {err}"); 
+        File::create(filename.to_string() + ".swap").unwrap();
+        "".to_string()
+    });
+    /*let mut file = File::options()
+        .write(true)
+        .open(&swap_path)
+        .unwrap_or_else(|err| {
+            eprintln!("ERROR: Could not open file: {err}"); 
+            File::create(filename).unwrap()
+        });*/
+    let mut content_lines: Vec<_> = file_content
+        .lines()
+        .collect();
+    println!("Lines: {content_lines:?}");
+    let mut changes: bool = false;
+    let content_lines_processed: Vec<String> = content_lines
+        .iter()
+        .map(|line| {
+            let mut line_processed = line;
+            if line.to_lowercase() == name.to_lowercase() {
+                line_processed = &newName;
+                changes = true;
+            }
+            //*line.push_str("\n");
+            line_processed.to_string() + "\n"
+        })
+        .collect();
+    println!("Processed: {:?}", content_lines_processed);
+    if changes {
+        let content_string = content_lines_processed.join("");
+        let content_bytes = content_string.as_bytes();
+        //file.write_all(content_bytes);
+        override_file(filename, content_string);
+    } else {
+        println!("Nothing changed");
+    }
+    /*copy(&swap_path, &path);
+    remove_file(&swap_path);*/
+    println!("Lines: {content_lines:?}");
+    println!("{file_content}");
+}
+fn override_file(filename: &str, content: String) {
+    let swap_path = format!("{SRC_DIR}/{filename}.swap");
+    let path = format!("{SRC_DIR}/{filename}");
+    copy(&path, &swap_path);
+    let mut file = File::options()
+        .write(true)
+        .open(&swap_path)
+        .unwrap_or_else(|err| {
+            eprintln!("ERROR: Could not open file: {err}"); 
+            File::create(filename).unwrap()
+        });
+    write!(file, "{content}");
+    copy(&path, &swap_path);
+    remove_file(&swap_path);
 }
--- src/script.js
+++ src/script.js
@@ -6,35 +6,69 @@ const inputButton = document.getElementById("inputButton");
 
 async function validate(name) {
 	if (name != "" && name != null) {
-		localStorage.setItem("name", name);
-		inputDiv.style.opacity = 0;
-		validationDiv.style.opacity = 1;
-		inputDiv.style.zIndex = 0;
-		validationDiv.style.zIndex = 1;
+		localStorage.setItem("newName", name);
 		let response = await fetch("validate", {
 			method: "POST",
 			body: nameElement.value
 		});
 		let text = await response.text();
+		console.log(text);
+		if (text == "success") {
+			inputDiv.style.opacity = 0;
+			validationDiv.style.opacity = 1;
+			inputDiv.style.zIndex = 0;
+			validationDiv.style.zIndex = 1;
+		} else {
+			alert("That name is already taken");
+		}
 	} else {
 		alert("Please input your name again");
 	}
 }
-async function switchName() {
+async function changeName() {
 	validationDiv.style.opacity = 0;
 	validationDiv.style.zIndex = 0;
 	inputDiv.style.opacity = 1;
 	inputDiv.style.zIndex = 1;
-	inputButton.onclick = "confirmSwitch()";
+	inputButton.onclick = confirmChange;
 }
-async function confirmSwitch() {
-	console.log("confirming");
-	let confirmation = await validate(nameElement.value);	
-	if (confirmation == "pass") {
-		fetch("deleteName", {
-			body: lsName
+async function confirmChange() {
+	let name = localStorage.getItem("name");
+	localStorage.setItem("newName", nameElement.value);
+	let newName = localStorage.getItem("newName");
+	if (newName != "" && newName != null) {
+		console.log("confirming");
+		let newName = localStorage.getItem("newName");
+		let response = await fetch("changeName", {
+			method: "PUT",
+			body: `${name}&${newName}`
 		});
+		if (response.ok && newName != undefined && newName != "") {
+			console.debug(localStorage.getItem("newName"));
+			console.log(localStorage.getItem("name"));
+			localStorage.setItem("name", localStorage.getItem("newName"));
+		} else {
+			console.log("no ok");
+		}
+		let text = await response.text();
+	} else {
+		alert("Please input your name again");
 	}
+	/*console.log(confirmation);
+	if (confirmation == "pass") {
+		let response = fetch("deleteName", {
+			method: "POST",
+			body: `${localStorage.getItem("name")}&${localStorage.getItem("newName")}`
+		}).then((response) => {
+			if (response.ok) {
+				console.log(localStorage.getItem("newName"));
+				console.log(localStorage.getItem("name"));
+				localStorage.setItem("name", localStorage.getItem("newName"));
+			} else {
+				console.log("no ok");
+			}
+		});
+	}*/
 }
 /*function hide(id) {
 	let div = document.getElementById(id);
@@ -58,6 +92,6 @@ if (lsName != null && lsName != "") {
 	inputDiv.style.zIndex = 1;
 	validationDiv.style.zIndex = 0;
 }
-let html = hide("validation");
-show ("validation", html);
+//let html = hide("validation");
+//show ("validation", html);
 
